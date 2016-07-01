51b3c6839061b7906b76d15c4c8a97dde5cc6770
yawnt <me@yawnt.com>
2016-07-01 15:24:30 +0200

    fix: licensing

--- src/main/scala/eu/unicredit/trafalgar/Trafalgar.scala
+++ src/main/scala/eu/unicredit/trafalgar/Trafalgar.scala
@@ -1,9 +1,21 @@
+/* Copyright 2016 UniCredit S.p.A.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package eu.unicredit.trafalgar
 
-import akka.actor.ActorSystem
-
 import akka.stream._
-import akka.stream.ActorMaterializer
 import akka.stream.scaladsl._
 import akka.stream.FlowShape
 
@@ -73,11 +85,4 @@ object Trafalgar {
     FlowShape(broadcastElems.in, merge.out)
   }).map(_ => new Exception("Service is misbehaving"))
 
-}
-
-object Main extends App {
-  implicit val system = ActorSystem("main")
-  implicit val mat = ActorMaterializer()
-
-  Source(1 to 100).map(_.toDouble).via(Trafalgar.flow).runWith(Sink.foreach(println))
 }
